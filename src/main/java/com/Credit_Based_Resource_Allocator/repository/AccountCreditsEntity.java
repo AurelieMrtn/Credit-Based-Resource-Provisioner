8fda5896566206939260083013f0d68045ec14e2
Aurélie Martin <aurelie.martin95@hotmail.fr>
2026-01-14 17:07:40 +0100

    Feat(repositories): entities and repositories added

---
+++ src/main/java/com/Credit_Based_Resource_Allocator/repository/AccountCreditsEntity.java
@@ -0,0 +1,29 @@
+package com.Credit_Based_Resource_Allocator.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class AccountCreditsEntity {
+    @Id
+    private String accountId;
+    private BigDecimal amount;
+
+    public AccountCreditsEntity(String accountId, BigDecimal amount) {
+        this.accountId = accountId;
+        this.amount = amount;
+    }
+
+    public AccountCreditsEntity() {
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+}
---
+++ src/main/java/com/Credit_Based_Resource_Allocator/repository/AccountCreditsRepository.java
@@ -0,0 +1,8 @@
+package com.Credit_Based_Resource_Allocator.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountCreditsRepository extends CrudRepository<AccountCreditsEntity, String> {
+
+}
+
